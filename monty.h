2cbaad5124cbd4f5042b6424395d38f21eed011d
Sado1111 <sadohannahfabohns@gmail.com>
2024-01-31 09:39:44 -0800

    Became betty compliant with indentation of monty.h and then specified vood as the parameters of push() & pull()

--- monty.h
+++ monty.h
@@ -15,9 +15,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -30,12 +30,12 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push();
-void pull();
+void push(void);
+void pull(void);
 
 
 #endif /* MONTY_H */
